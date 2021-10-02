11096f7b5358b81ba9ecb59777e0ccde34464c8a
savshenal123 <91730781+savshenal123@users.noreply.github.com>
2021-10-02 09:05:23 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,13 @@
-from tkinter import *
-
-window = Tk()
-
-window.title(" chat app")
-
-window.mainloop():
-input(int:"enter value")
+x= input(str("enter your name : ")
+z = input(int("enter your age : "))
+if z =< 50 and z=>30:
+         print("nama: ",x "age :",z "you are in middle age")
+elif z =< 30 and z=>18:
+         print("nama: ",x "age :",z "you are in younger")
+elif z =<18 and z=> 13:
+         print("nama: ",x "age :",z "you are teenager")
+elif z =< 13:
+         print("nama: ",x "age :",z "you are a child")
+else:
+         print("nama: ",x "age :",z "you are a elder")
 
